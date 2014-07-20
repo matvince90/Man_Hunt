7c4a94b6f6f52f5c496b47d4899ee85c17473622
DarthUnicorn <arieldiaz92@gmail.com>
2014-07-20 14:03:11 -0400

    Major refactoring to PowerUpCommand class
    ADDED:
    -Player variable
    -PowerUp variable
    -GameMatchSingleton variable
    -execute() reworked to include powerUp type and cooldown implementation

--- Manhunt/src/com/fiu/manhunt/GameModel/PowerUpCommand.java
+++ Manhunt/src/com/fiu/manhunt/GameModel/PowerUpCommand.java
@@ -2,32 +2,57 @@ package com.fiu.manhunt.GameModel;
  
   class PowerUpCommand implements Command {
  
-  private  PowerUp powerUp = null;
-   
-  public PowerUpCommand (PowerUp powerUp) { 
+  private Player player;
+  private PowerUp powerUp;
+  private GameMatchSingleton gameMatch;
+  
+  public PowerUpCommand (Player player, PowerUp powerUp) { 
+	  this.player = player;
 	  this.powerUp = powerUp;
-  }; 
+	  this.gameMatch = GameMatchSingleton.getInstance();
+  }
  
-  public void execute()
-  {
-	  this.powerUp.setIsActive(true);
-	  
-	  //GameMatch is modified due to the PowerUp
-	  GameMatchSingleton gameMatch = GameMatchSingleton.getInstance();
-	  
-	  long startTime = System.currentTimeMillis();
-	  int duration = this.powerUp.getDuration();
-	  
-	  while ((System.currentTimeMillis()-startTime)< duration && this.powerUp.IsActive()){
-		  //execution of the powerUp
+  public void execute() 
+  { 
+	  //Cool down: making sure cool down is not active
+	  //if the time that has passed since last power up use is greater than the cool down, then execute
+	  if(System.currentTimeMillis() - player.getPowerUpLastUsedTimestamp() > powerUp.getCoolDown())
+	  {
+		  this.powerUp.setIsActive(true);
 		  
+		  //Removes current powerUp from User
+		  this.player.removePowerUp(powerUp); 
 		  
+		  long startTime = System.currentTimeMillis();
+		  long powerUpDuration = this.powerUp.getDuration();
 		  
+		  //If the PowerUp selected was 'Cloak'
+		  if(this.powerUp.getType() == PowerUp.PowerUps.Cloak)
+		  {
+			  //Hides location indefinitely until location is set to be displayed again
+			  this.player.hideLocation(); 
+			  
+			  while ((System.currentTimeMillis()-startTime)< powerUpDuration)
+			  {
+				  try{
+					  Thread.sleep(1000);
+				  }
+				  catch(InterruptedException e)
+				  {
+					  System.out.println(e);
+				  }
+			  }
+			  
+			  this.player.getLocation(); 
+		  }
+		  else
+		  {
+			  //Space for future PowerUp Implementations
+		  }
 		  
-		  
+		  this.powerUp.setIsActive(false); 
 	  }
 	  
-	  this.powerUp.setIsActive(false);
   }
  
 }
