97422d2ffac7d4e5b85d37e0fba6479e0824dfa6
Justin Phillips <me@justinphillips.co>
2014-07-19 20:12:57 -0400

    Refactored the web service to include a less coupled approach. The web service implements Javax.ws.rx with the attached jar api. Also included is a runnable class for instantiating and starting the controller service.

--- ManhuntServer/src/ManhuntServer.java
+++ ManhuntServer/src/ManhuntServer.java
@@ -1,10 +1,16 @@
-
-
+/**
+ * ManHuntServer class spawns a thread which invokes the server application.
+ * @author Team 3
+ *
+ */
 public class ManhuntServer {
 
+	/**
+	 * Main entry point for the server application
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		(new Thread(new ServiceRunnable())).start();
 	}
 
 }
---
+++ ManhuntServer/src/ServiceRunnable.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Team 3
+ *
+ */
+public class ServiceRunnable implements Runnable {
+
+	private ServerSide.GameLogicController _gameLogicController;
+	public void run() {
+		this._gameLogicController = new ServerSide.GameLogicController();
+	}
+
+}
