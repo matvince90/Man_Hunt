034b23b53e282f573a6fb2f4848e9f36e98e83c1
Musa V. Ahmed <mahme012@fiu.edu>
2014-07-27 17:00:52 -0400

    initial commit

---
+++ ManhuntDB/manhunt.sql
@@ -0,0 +1,26 @@
+﻿DROP TABLE IF EXISTS GameMatchPlayers;
+DROP TABLE IF EXISTS BanList;
+DROP TABLE IF EXISTS GameMatch;
+DROP TABLE IF EXISTS Player;
+
+CREATE TABLE Player           (pid        SERIAL,
+                               email      CHAR(30),
+                               latitude   REAL,
+                               longitude  REAL,
+                               status     INTEGER,
+                               PRIMARY KEY (pid));
+
+CREATE TABLE GameMatch        (gid        SERIAL,
+                               startTime  INTEGER,
+                               active     BOOLEAN,
+                               PRIMARY KEY (gid));
+
+CREATE TABLE BanList          (pid         INTEGER,
+                               PRIMARY KEY (pid));
+
+CREATE TABLE GameMatchPlayers (pid        INTEGER,
+                               gid        INTEGER,
+                               PRIMARY KEY (pid, gid),
+                               FOREIGN KEY (pid) REFERENCES Player,
+                               FOREIGN KEY (gid) REFERENCES GameMatch
+                               ON DELETE CASCADE);
