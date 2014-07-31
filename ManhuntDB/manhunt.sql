4cdc2650a53a2d5fac219d6de73df3e48df296e2
Musa V. Ahmed <mahme012@fiu.edu>
2014-07-30 21:01:02 -0400

    table keys are serial

--- ManhuntDB/manhunt.sql
+++ ManhuntDB/manhunt.sql
@@ -3,23 +3,23 @@ DROP TABLE IF EXISTS BanList;
 DROP TABLE IF EXISTS GameMatch;
 DROP TABLE IF EXISTS Player;
 
-CREATE TABLE Player           (pid        SERIAL,
+CREATE TABLE Player           (pid        SERIAL NOT NULL,
                                email      CHAR(30),
                                latitude   REAL,
                                longitude  REAL,
-                               status     INTEGER,
+                               type     INTEGER,
                                PRIMARY KEY (pid));
 
-CREATE TABLE GameMatch        (gid        SERIAL,
+CREATE TABLE GameMatch        (gid        SERIAL NOT NULL,
                                startTime  INTEGER,
                                active     BOOLEAN,
                                PRIMARY KEY (gid));
 
-CREATE TABLE BanList          (pid         INTEGER,
+CREATE TABLE BanList          (pid         INTEGER NOT NULL,
                                PRIMARY KEY (pid));
 
-CREATE TABLE GameMatchPlayers (pid        INTEGER,
-                               gid        INTEGER,
+CREATE TABLE GameMatchPlayers (pid        INTEGER NOT NULL,
+                               gid        INTEGER NOT NULL,
                                PRIMARY KEY (pid, gid),
                                FOREIGN KEY (pid) REFERENCES Player,
                                FOREIGN KEY (gid) REFERENCES GameMatch
