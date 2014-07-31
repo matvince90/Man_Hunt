2ca7f66715dc58897fe9b810bf70371c1cebe7f4
Justin Phillips <me@justinphillips.co>
2014-07-31 11:12:30 -0400

    Added bad test cases

--- ManhuntServerTest/src/main/java/com/fiu/manhunt/test/driver/TestDriver.java
+++ ManhuntServerTest/src/main/java/com/fiu/manhunt/test/driver/TestDriver.java
@@ -13,58 +13,113 @@ import com.google.gson.Gson;
 public class TestDriver {
 	private static final int NUM_TESTS = 25;
 	private static Random _rnd;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		_rnd = new Random();
 		_rnd.setSeed(0);
-		
+
 		try {
-		TestController();
-		} catch(Exception E) {
+			TestController();
+		} catch (Exception E) {
 			System.out.println("Something major broke.");
 		}
 	}
-	
-	private static void TestController() throws FileNotFoundException, UnsupportedEncodingException {
+
+	private static void TestController() throws FileNotFoundException,
+			UnsupportedEncodingException {
 		ControllerTest ct = new ControllerTest();
-		
+
 		PrintWriter writer = createLog("ControllerTest");
-		
+
 		PlayerMessageData pmd = new PlayerMessageData();
 		PlayerMessageData.PlayerData pd = pmd.new PlayerData();
 		Gson gson = new Gson();
-		
-		
-		
-		for(int i = 0; i < NUM_TESTS; i++) {
-			// test within a range of four e-mails (this way we hit 4 new players and Num_test - 4 updates)
+
+		// good test cases
+		for (int i = 0; i < NUM_TESTS; i++) {
+			// test within a range of four e-mails (this way we hit 4 new
+			// players and Num_test - 4 updates)
 			pd.set_email("test" + (i % 4) + "@somewhere.com");
-			
+
 			// random locations.
-			pd.set_lat((float)1.2234 * (float)getRandomRange(1, 25));
-			pd.set_long((float)1.2234 * (float)getRandomRange(5, 40));
-			
+			pd.set_lat((float) 1.2234 * (float) getRandomRange(1, 25));
+			pd.set_long((float) 1.2234 * (float) getRandomRange(5, 40));
+
 			// expected input
 			String playerJsonData = gson.toJson(pd);
 			writer.println("\n--Test " + i + " input--\n");
 			writer.println(playerJsonData);
-			
+
 			// write out results
 			writer.println("\n--Test " + i + " output--\n");
 			writer.println(ct.TestPlayerUpdate(playerJsonData));
 		}
+
+		// bad test cases
+		for (int i = 0; i < NUM_TESTS; i++) {
+			// test with valid e-mail, invalid e-mail, and no e-mail
+			// test1@somewhere.com should be in ban list.
+			if (i % 3 == 0)
+				pd.set_email("test" + (i % 4) + "@somewhere.com");
+			else if (i % 3 == 1)
+				pd.set_email("test" + (i % 4));
+			else
+				pd.set_email("");
+
+			pd.set_lat((float) 1.2234 * (float) getRandomRange(1, 25));
+			pd.set_long((float) 1.2234 * (float) getRandomRange(5, 40));
+
+			// expected input
+			String playerJsonData = gson.toJson(pd);
+			writer.println("\n--Bad E-mail Test " + i + " input--\n");
+			writer.println(playerJsonData);
+
+			// write out results
+			writer.println("\n--Bad E-mail Test " + i + " output--\n");
+			writer.println(ct.TestPlayerUpdate(playerJsonData));
+		}
+
+		// bad localtion
+		for (int i = 0; i < NUM_TESTS; i++) {
+
+			pd.set_email("test" + (i % 4) + "@somewhere.com");
+
+			if (i % 3 == 0) {
+				pd.set_lat(-1);
+				pd.set_long(-1);
+			}
+			else if (i % 3 == 1) {
+				pd.set_lat(0);
+				pd.set_long(0);
+			}
+			else {
+				pd.set_lat((float)10000000000000000000000000000000000000000000.0 * (i % 5));
+				pd.set_long((float)1000000000000000000000000000000000000000000.0 * (i % 5));
+			}
+
+			// expected input
+			String playerJsonData = gson.toJson(pd);
+			writer.println("\n--Bad E-mail Test " + i + " input--\n");
+			writer.println(playerJsonData);
+
+			// write out results
+			writer.println("\n--Bad E-mail Test " + i + " output--\n");
+			writer.println(ct.TestPlayerUpdate(playerJsonData));
+		}
 		writer.close();
 	}
-	
+
 	// support functions
-	private static PrintWriter createLog(String prefix) throws FileNotFoundException, UnsupportedEncodingException {
+	private static PrintWriter createLog(String prefix)
+			throws FileNotFoundException, UnsupportedEncodingException {
 		int i = 0;
 		String fileName = null;
-		while(i >= 0 && i <= 100) {
-			File log = new File(prefix + "-"+ i++ +".txt");
-			if(!log.exists()) {
+		while (i >= 0 && i <= 100) {
+			File log = new File(prefix + "-" + i++ + ".txt");
+			if (!log.exists()) {
 				fileName = log.getName();
 				break;
 			}
@@ -72,7 +127,7 @@ public class TestDriver {
 		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 		return writer;
 	}
-	
+
 	private static int getRandomRange(int min, int max) {
 		return (_rnd.nextInt((max - min) + 1) + min);
 	}
